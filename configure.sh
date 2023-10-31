a6113ffd12209820b4e70d28dab48ec87c98443d
wszx3394 <47415641+wszx3394@users.noreply.github.com>
2023-10-31 17:06:24 +0800

    更新configure.sh配置

--- configure.sh
+++ configure.sh
@@ -2,7 +2,8 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.5.1
-#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.15.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Hello World
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -17,7 +18,6 @@
 #通常这两个就够了
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
-echo "src-git lede https://github.com/coolsnowwolf/luci" >> feeds.conf.default
 # 替换默认主题
 #rm -rf package/lean/luci-theme-argon 
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
