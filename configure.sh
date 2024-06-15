59e1fc72098c2154895fdbee46a976265d5ca22a
wszx3394 <47415641+wszx3394@users.noreply.github.com>
2024-06-15 23:43:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -13,25 +13,16 @@ sed -i 's/192.168.15.1/192.168.5.1/g' package/base-files/files/bin/config_genera
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-#echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-#echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
 
 # 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 #设置内核版本为5.10
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
-#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
 
 #拉取最新版golang编译
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-#临时
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
-make menuconfig
