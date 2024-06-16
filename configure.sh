5ddf900bfb7b7c03de4853465e75d284c2bcc315
wszx3394 <47415641+wszx3394@users.noreply.github.com>
2024-06-17 00:19:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 # 修改默认系统设置
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate 
 sed -i 's/192.168.15.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # passwall
