cc52cc7dd5b5cfa28bbf6f016fa490447733b351
wszx3394 <47415641+wszx3394@users.noreply.github.com>
2024-09-24 02:16:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -13,8 +13,10 @@ sed -i 's/192.168.15.1/192.168.5.1/g' package/base-files/files/bin/config_genera
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+#echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+#echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
